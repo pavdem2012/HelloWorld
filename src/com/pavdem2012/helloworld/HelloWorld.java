a5c81b268963398d36b7bb9729f89aa8cc7b3acb
pavdem2012 <pavdem2012@gmail.com>
2021-02-09 00:55:46 +1100

    initial commit.

---
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.pavdem2012.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+         System.out.println("Hello World!!!! да ну нафиг так бывает???");
+
+	}
+
+}

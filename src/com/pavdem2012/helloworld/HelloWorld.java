25f6d199f95506dd9cbe2bc8161d8ab16406689a
pavdem2012 <pavdem2012@gmail.com>
2021-02-15 01:22:19 +1100

    Extract message with Boolean variables with formating text

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -2,26 +2,24 @@ package com.pavdem2012.helloworld;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		float i = 6; 
-		float j = 7; 
-		float answer =i /j; 
-		
-	String message = "Hello World!!!! Да ну нафиг так бывает???";
-	
-	String stringAnswer = message + " " + answer;
-	
-	
-	boolean isMoreThanOne ;
-	if (answer > 1) {
-		isMoreThanOne = true;
-	} else {
-		isMoreThanOne = false;	
-	}
-		
-         System.out.println(message);
-         System.out.println(stringAnswer + " " + isMoreThanOne);       
+    public static void main(String[] args) {
+    
+        float i = 6; 
+        float j = 7; 
+        float answer =i /j; 
 
-	}
+        String message = "Hello World!!!! Да ну нафиг так бывает???";
 
+        String stringAnswer = message + " " + answer;
+
+        boolean isMoreThanOne ;
+        if (answer > 1) {
+            isMoreThanOne = true;
+        } else {
+            isMoreThanOne = false;	
+        }
+
+        System.out.println(message);
+        System.out.println(stringAnswer + " " + isMoreThanOne); 
+    }
 }

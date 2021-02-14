13468d302f8baccc0daced1e67b798fbce5284fa
pavdem2012 <pavdem2012@gmail.com>
2021-02-14 21:36:53 +1100

    Extract message with Boolean variables

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -5,11 +5,22 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		float i = 6; 
 		float j = 7; 
-		float answer =i /j; 				
+		float answer =i /j; 
+		
 	String message = "Hello World!!!! Да ну нафиг так бывает???";
+	
 	String stringAnswer = message + " " + answer;
+	
+	
+	boolean isMoreThanOne ;
+	if (answer > 1) {
+		isMoreThanOne = true;
+	} else {
+		isMoreThanOne = false;	
+	}
+		
          System.out.println(message);
-         System.out.println(stringAnswer);       
+         System.out.println(stringAnswer + " " + isMoreThanOne);       
 
 	}
 

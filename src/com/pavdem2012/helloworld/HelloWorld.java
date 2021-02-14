353295f6624c6c0339248885a4220f731e0e1f97
pavdem2012 <pavdem2012@gmail.com>
2021-02-14 20:59:38 +1100

    Extract message into floating point arithmetic; non-floating point option

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -3,9 +3,9 @@ package com.pavdem2012.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int i = 6; 
-		int j = 7; 
-		float answer =(float)i /(float)j; 				
+		float i = 6; 
+		float j = 7; 
+		float answer =i /j; 				
 	String message = "Hello World!!!! Да ну нафиг так бывает???";
 	String stringAnswer = message + " " + answer;
          System.out.println(message);

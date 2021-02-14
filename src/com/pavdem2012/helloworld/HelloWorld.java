2f7e29541e140a510764c18651e3a958bb9da0b3
pavdem2012 <pavdem2012@gmail.com>
2021-02-14 20:54:18 +1100

    Extract a message into floating point arithmetic; variant with casting to float

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		int i = 6; 
 		int j = 7; 
-		int answer = i * j; 				
+		float answer =(float)i /(float)j; 				
 	String message = "Hello World!!!! Да ну нафиг так бывает???";
 	String stringAnswer = message + " " + answer;
          System.out.println(message);

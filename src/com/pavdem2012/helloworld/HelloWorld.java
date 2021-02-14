8505b8a2484a87c9c8a2e568a8a687eb7a8578d8
pavdem2012 <pavdem2012@gmail.com>
2021-02-14 14:58:26 +1100

    Extract message to the string variable

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -3,8 +3,8 @@ package com.pavdem2012.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-         System.out.println("Hello World!!!! Да ну нафиг так бывает???");
+	String message = "Hello World!!!! Да ну нафиг так бывает???";
+         System.out.println(message);
 
 	}
 

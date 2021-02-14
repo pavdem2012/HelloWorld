76891d6e465b0ff64e97a441f5cedb1f9cf4f101
pavdem2012 <pavdem2012@gmail.com>
2021-02-14 20:06:46 +1100

    Extract message in arithmetic operation with integer variables

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -3,10 +3,12 @@ package com.pavdem2012.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int i = 42; 
+		int i = 6; 
+		int j = 7; 
+		int answer = i * j; 
 	String message = "Hello World!!!! Да ну нафиг так бывает???";
          System.out.println(message);
-         System.out.println(i);       
+         System.out.println(answer);       
 
 	}
 

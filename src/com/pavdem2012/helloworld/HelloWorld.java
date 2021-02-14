ab2a515c280d6e9599593aa2fffd8b82fcb08fe4
pavdem2012 <pavdem2012@gmail.com>
2021-02-14 19:52:27 +1100

    Extract message to the integer variable

--- src/com/pavdem2012/helloworld/HelloWorld.java
+++ src/com/pavdem2012/helloworld/HelloWorld.java
@@ -3,8 +3,10 @@ package com.pavdem2012.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		int i = 42; 
 	String message = "Hello World!!!! Да ну нафиг так бывает???";
          System.out.println(message);
+         System.out.println(i);       
 
 	}
 
